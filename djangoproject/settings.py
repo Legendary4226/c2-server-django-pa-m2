2688bd0ab561eaf18497e55629ce0a4ceb790018
HexteckGate <contact@hexteckgate.ovh>
2026-05-27 23:41:48 +0200

    Fix debug mode in prod

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2_!2ek$vyvv*x7gn(^%2ns=pg1g&b=t-7^y^$d+6jvlrg8#n)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
