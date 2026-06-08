5451f7455fd2a43ef21cc7b16072b3507651a98f
HexteckGate <contact@hexteckgate.ovh>
2026-06-08 22:05:15 +0200

    fix: processing queue was failing and stuck

--- djangoproject/management/commands/handle_finished_job.py
+++ djangoproject/management/commands/handle_finished_job.py
@@ -15,25 +15,27 @@ class Command(BaseCommand):
 
         print(f"Processing queue element #{queue.id} Job #{queue.job_id}")
 
-        job = queue.job
-        machine = queue.job.infected_machine
-        data_folder = job.data_folder_path()
+        try:
+            job = queue.job
+            data_folder = job.data_folder_path()
 
-        job.finished_at = timezone.now()
-        job.save()
+            job.finished_at = timezone.now()
+            job.save()
 
-        chunks = job.get_sorted_chunks()
-        with open(f"{data_folder}/extracted", "w") as extracted:
-            for chunkFile in chunks:
-                with open(f"{data_folder}/{chunkFile}", "r") as chunk:
-                    data = chunk.read().upper()
-                    if (len(data) % 8) > 0:
-                        data += '=' * (len(data) % 8)
-                    extracted.write(
-                        base64.b32decode(data).decode('UTF-8')
-                    )
+            chunks = job.get_sorted_chunks()
+            with open(f"{data_folder}/extracted", "w") as extracted:
+                for chunkFile in chunks:
+                    with open(f"{data_folder}/{chunkFile}", "r") as chunk:
+                        data = chunk.read().upper()
+                        if (len(data) % 8) > 0:
+                            data += '=' * (len(data) % 8)
+                        extracted.write(
+                            base64.b32decode(data).decode('UTF-8')
+                        )
+
+            print(f"End process queue #{queue.id} Job #{queue.job_id}: {len(chunks)} chunks merged in extracted")
+        except Exception as e:
+            print(f"Failed to process merge of chunks for queue #{queue.id} Job #{queue.job_id} {e}")
 
         queue.processed = True
         queue.save()
-
-        print(f"End process queue #{queue.id} Job #{queue.job_id}: {len(chunks)} chunks merged in extracted")
